5d6a4a634588fd89bd468a4221269a9c8241f35e
Sonu <loginsonu.0.0.1@gmail.com>
2023-04-30 18:34:26 +0530

    play/pause button and prev and next button

--- app/src/main/java/com/example/musictherapy/MediaPlayerr.kt
+++ app/src/main/java/com/example/musictherapy/MediaPlayerr.kt
@@ -1,19 +1,17 @@
 package com.example.musictherapy
-import android.annotation.SuppressLint
+
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
-import android.os.Message
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-import com.example.musictherapy.R
 
 class MediaPlayerr : AppCompatActivity() {
 
     private var mediaPlayer: MediaPlayer? = null
     private lateinit var seekBar: SeekBar
+    private lateinit var playPauseButton: Button
     private var handler: Handler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +19,7 @@ class MediaPlayerr : AppCompatActivity() {
         setContentView(R.layout.activity_media_player)
 
         // Find views
-        val playButton = findViewById<ImageView>(R.id.playButton)
-        val pauseButton = findViewById<ImageView>(R.id.pauseButton)
+        playPauseButton = findViewById(R.id.playPauseButton)
         seekBar = findViewById(R.id.seekBar)
 
         // Get music file path from intent
@@ -34,16 +31,17 @@ class MediaPlayerr : AppCompatActivity() {
                 prepare()
             }
 
-            // Setup the play button to play the media
-            playButton.setOnClickListener {
-                mediaPlayer?.start()
-                handler?.post(updateSeekBar)
-            }
-
-            // Setup the pause button to pause the media
-            pauseButton.setOnClickListener {
-                mediaPlayer?.pause()
-                handler?.removeCallbacks(updateSeekBar)
+            // Setup the play/pause button
+            playPauseButton.setOnClickListener {
+                if (mediaPlayer?.isPlaying == true) {
+                    mediaPlayer?.pause()
+                    handler?.removeCallbacks(updateSeekBar)
+                    playPauseButton.text = "Play"
+                } else {
+                    mediaPlayer?.start()
+                    handler?.post(updateSeekBar)
+                    playPauseButton.text = "Pause"
+                }
             }
 
             // Setup the SeekBar
